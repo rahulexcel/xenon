58e4a55f4640e76566f3558fb3f3149cffc995cf
rahulexcel <prabhunath@excellencetechnologies.in>
2015-12-24 15:21:37 +0530

    calander is added timepicker is added

--- app/js/httpinterceptor.factory.js
+++ app/js/httpinterceptor.factory.js
@@ -1,25 +1,21 @@
+(function() {
+   'use strict';
+angular.module('xenon-app')
+  .factory('myInterceptor', function (localStorageService) {
+            var requestInterceptor = {
+                request: function (config) {
+                	var currentUser = localStorageService.get('userData');
+       				var accessToken = currentUser.token;
 
-// (function() {
-//    'use strict';
-// angular.module('xenon-app')
-//   .factory('myInterceptor', function ($q) {
-//             var requestInterceptor = {
-//                 request: function (config) {
-                                        
-//                         // config.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-                    
-//                     var deferred = $q.defer();
-
-//                     config.transformRequest = function (obj) {
-//                         var str = [];
-//                         for (var p in obj)
-//                             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-//                         return str.join("&");
-//                     };
-//                     deferred.resolve(config);
-//                     return deferred.promise;
-//                 }
-//             };
-//             return requestInterceptor;
-//         });
-//   })();
+        			if (accessToken) {
+           				 config.headers = {
+				           'Content-Type': 'application/json',
+				           'Authorization': 'Bearer ' + accessToken
+				         };
+       				 }
+       				 return config;
+                }
+            };
+            return requestInterceptor;
+        });
+  })();
