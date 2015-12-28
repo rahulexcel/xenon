ea8cf6566e03ade596d84b4d71528945e2f61c4b
rahulexcel <prabhunath@excellencetechnologies.in>
2015-12-28 13:35:43 +0530

    location id api is implemented edit/update is working now in store info

--- app/storeinfo/storeinfo.controller.js
+++ app/storeinfo/storeinfo.controller.js
@@ -11,70 +11,123 @@ function storeinfoCtrl($scope, $log, FileUploader, storeinfoFactory, localStorag
         mstep: [1, 5, 10, 15, 25, 30]
     };
     $scope.openingtime = function() {
-        $log.log('Time changed to: ' + $scope.open);
+        $log.log('open time ' + $scope.open);
     };
     $scope.closingtime = function() {
-        $log.log('Time changed to: ' + $scope.close);
+        $log.log('Time close ' + $scope.close);
     };
 
 
-var userData = localStorageService.get("userData");
-console.log(userData.locations[0]);
-var a=userData.locations[0];
-console.log("userdata.location\n"+userData.locations);
-edit();
-function edit() {
-        var query = storeinfoLocationsIdFactory.update({
-            locationId: userData.locations[0]
+    var userData = localStorageService.get("userData");
+    console.log("location id at o \n" + userData.locations[0]);
+    var a = userData.locations[0];
+    console.log("locations in local storage\n" + userData.locations);
+
+
+    edit();
+
+    function edit() {
+        var query = storeinfoLocationsIdFactory.update({}, {
+            'locationid': userData.locations[0],
+            'lname': $scope.lname,
+            'ldesc': $scope.ldesc,
+            'lemail': $scope.lemail,
+            'llgo': $scope.llogo,
+            'laddr': $scope.laddr,
+            'lpostcode': $scope.lpostcode,
+            'lcity': $scope.city,
+            'lcountry': $scope.lcountry,
+            'lphone': $scope.lphone,
+            'llt': $scope.llt,
+            'lmessage': $scope.lmessage,
+            'lopentime': $scope.open,
+            'lclosetime': $scope.close
         });
         query.$promise.then(function(data) {
             console.log(data);
+            $scope.lname = data.data.lname;       
+            angular.element(document.querySelector('.CodeMirror-code pre span')).text(data.data.ldesc);
+            $scope.lemail = data.data.lemail;
+            $scope.llogo = data.data.llogo;
+            $scope.laddr = data.data.laddr;
+            $scope.lpostcode = data.data.lpostcode;
+            $scope.city = data.data.city;
+            $scope.lcountry = data.data.lcountry;
+            $scope.lphone = data.data.lphone;
+            $scope.llt = data.data.llt;
+            $scope.lmessage = data.data.lmessage;
+            $scope.open = data.data.lopentime;
+            $scope.close = data.data.lclosetime;
+
         });
 
     }
+    // eid();
+    // function eid() {
+    //     var userData = localStorageService.get("userData");
+    //     var eid = userData.eid;
+    //     console.log(eid);
+    //     var query1 = storeinfoLocationsFactory.get({
+    //         eid: eid
+    //     });
+    //     query1.$promise.then(function(data) {
+    //         console.log(data);
+    //     });
 
+    // }
 
-
-
-    eid();
-
-    function eid() {
-        var userData = localStorageService.get("userData");
-        var eid = userData.eid;
-        console.log(eid);
-        var query1 = storeinfoLocationsFactory.get({
-            eid: eid
-        });
-        query1.$promise.then(function(data) {
-            console.log(data);
-        });
-
-    }
     $scope.lsave = function() {
-      console.log('open'+$scope.open);
-       console.log('close'+$scope.close);
-        $scope.ldesc = angular.element(document.querySelector('#uikit_editor_2')).val();
-        var query = storeinfoLocationsIdFactory.update({
-            locationId: userData.locations[0],
-            lname: $scope.lname,
-            ldesc: $scope.ldesc,
-            lemail: $scope.lemail,
-            llgo: $scope.llogo,
-            laddr: $scope.laddr,
-            lpostcode: $scope.lpostcode,
-            lcity: $scope.city,
-            lcountry: $scope.lcountry,
-            lphone: $scope.lphone,
-            llt: $scope.llt,
-            lmessage: $scope.lmessage,
-            lopentime: $scope.open,
-            lclosetime: $scope.close
+      $scope.spinner=true;
+        var query = storeinfoLocationsIdFactory.update({}, {
+            'locationid': userData.locations[0],
+            'lname': $scope.lname,
+            'ldesc': $scope.ldesc,
+            'lemail': $scope.lemail,
+            'llgo': $scope.llogo,
+            'laddr': $scope.laddr,
+            'lpostcode': $scope.lpostcode,
+            'lcity': $scope.city,
+            'lcountry': $scope.lcountry,
+            'lphone': $scope.lphone,
+            'llt': $scope.llt,
+            'lmessage': $scope.lmessage,
+            'lopentime': $scope.open,
+            'lclosetime': $scope.close
         });
         query.$promise.then(function(data) {
-            console.log(data);
-
+            $scope.spinner=false;
         });
     }
+
+
+
+
+    // $scope.lsave = function() {
+    //   console.log('open'+$scope.open);
+    //    console.log('close'+$scope.close);
+    //     $scope.ldesc = angular.element(document.querySelector('#uikit_editor_2')).val();
+    //     var query = storeinfoFactory.save({
+    //         locationId: userData.locations[0],
+    //         lname: $scope.lname,
+    //         ldesc: $scope.ldesc,
+    //         lemail: $scope.lemail,
+    //         llgo: $scope.llogo,
+    //         laddr: $scope.laddr,
+    //         lpostcode: $scope.lpostcode,
+    //         lcity: $scope.city,
+    //         lcountry: $scope.lcountry,
+    //         lphone: $scope.lphone,
+    //         llt: $scope.llt,
+    //         lmessage: $scope.lmessage,
+    //         lopentime: $scope.open,
+    //         lclosetime: $scope.close
+    //     });
+    //     query.$promise.then(function(data) {
+    //         console.log(data.data.lpostcode);
+    //         edit();
+
+    //     });
+    // }
 
 
     var uploader = $scope.uploader = new FileUploader({
--- app/storeinfo/storeinfolocationid.factory.js
+++ app/storeinfo/storeinfolocationid.factory.js
@@ -1,8 +1,30 @@
 (function() {
-   'use strict';
-   angular.module('xenon-app')
-       .factory('storeinfoLocationsIdFactory', storeinfoLocationsIdFactory);
-   function storeinfoLocationsIdFactory($resource, Configurations) {
-       return $resource(Configurations.Hostserver+'/loc/:locationid/:ldesc/:lemail/:laddr', {},{ 'update': { method:'PUT' }});
+    'use strict';
+    angular.module('xenon-app')
+        .factory('storeinfoLocationsIdFactory', storeinfoLocationsIdFactory);
+
+    function storeinfoLocationsIdFactory($resource, Configurations) {
+        return $resource(Configurations.Hostserver + '/loc/:locationid', {
+            'locationid': '@locationid'
+        }, {
+            'update': {
+                method: 'PUT',
+                params: {
+                    lname: '@lname',
+                    ldesc: '@ldesc',
+                    lemail: '@lemail',
+                    llgo: '@llgo',
+                    ladd: '@ladd',
+                    lpostcode: '@lpostcode',
+                    lcity: '@lcity',
+                    lcountry: '@lcountry',
+                    lphone: '@lphone',
+                    llt: '@llt',
+                    lmessage: '@lmessage',
+                    lopentime: '@lopentime',
+                    lclosetime: '@lclosetime'
+                }
+            }
+        });
     };
-})();                                                                                    
+})();
