088b829ee1d4ef09b4d23f50999d12a2ba4fa68a
rahulexcel <yuvraj@excellencetechnologies.in>
2016-01-05 19:46:55 +0530

    Modification in Paymaent page. api and window.open

--- app/payment/payment.controller.js
+++ app/payment/payment.controller.js
@@ -3,7 +3,7 @@
    
        angular.module('xenon-app')
        .controller('paymentController', paymentController);
-        function paymentController($scope, paymentFactory) {
+        function paymentController($scope, paymentFactory, localStorageService) {
        		console.log("Payment Page");
        		var query = paymentFactory.get();
             query.$promise.then(function(data) {
@@ -12,9 +12,12 @@
             $scope.activateStripe = function(){
             	console.log('activateStripe');
               //window.open ("https://protected-badlands-3499.herokuapp.com/payments/authorize");
+             var userData = localStorageService.get('userData');
+             var lid = userData.locations[0];
        		var query = paymentFactory.authorize();
             query.$promise.then(function(data) {
                         console.log(data);
+                        window.open (data.authorize_url+"?response_type="+data.response_type+"&scope="+data.scope+"&client_id="+data.client_id+"&state="+lid);
                     });
             };
 };
