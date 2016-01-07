3e745b9420ec280137cdabdaf3042216b19fde9c
rahulexcel <prabhunath@excellencetechnologies.in>
2016-01-07 17:58:53 +0530

    product add page: image uploading and editing is done

--- app/addproduct/addproduct.controller.js
+++ app/addproduct/addproduct.controller.js
@@ -4,18 +4,24 @@
        angular.module('xenon-app')
        .controller('addProductController', addProductController);
         function addProductController($scope, addProductFactory, Upload, localStorageService, $rootScope, productListFactory, productFactory, $state, imageUploadFactory) {
-       		console.log("Add Product Page");
+        //  console.log("Add Product Page");
+        var after_load_image_response;
+        var flag_for_cheking_add_or_edit=0;
+        var edit_Product_Id;
           if($rootScope.editProductId){
+            flag_for_cheking_add_or_edit=1;
             $scope.formSpinner = true;
             $scope.editForm = false;
-            console.log('Edit Product Id: '+$rootScope.editProductId);
+          //  console.log('Edit Product Id: '+$rootScope.editProductId);
             $scope.editThisProduct = true;
             $scope.saveProduct = false;
             var query = productListFactory.singleProduct({"productId": $rootScope.editProductId});
             query.$promise.then(function(data) {
-                        console.log(data);
+                        console.log(data);  
+                      $scope.picImage='http://s3.amazonaws.com/ordermagic/'+data.pimages[0];
+                      after_load_image_response=$scope.picImage;
                         $scope.productName = data.pname;
-                        angular.element(document.querySelector('.CodeMirror-code pre span')).text(data.pdesc);
+                        $scope.productDescription = data.pdesc;
                         $scope.productPrice = data.price;
                         $scope.productQuantity = data.pinv;
                         $scope.formSpinner = false;
@@ -30,10 +36,18 @@
             $scope.productQuantity = 'Infinite';
           };
           $scope.editProduct = function(editProductId){
+            edit_Product_Id=editProductId;
             $scope.spinner = true;
-            console.log(editProductId);
-            $scope.productDescription = angular.element(document.querySelector('.CodeMirror-code pre span')).text();
-            var userData = localStorageService.get('userData');
+             if($scope.picImage==after_load_image_response){
+                  edit_product_after_uploader_response();
+         
+            }else{
+                  $scope.spinner = true;
+                   upload($scope.picImage, 'https://protected-badlands-3499.herokuapp.com/prodfile');
+                 }
+          };
+          function edit_product_after_uploader_response(){
+             var userData = localStorageService.get('userData');
             var lid = userData.locations[0];
             if($scope.productQuantity == 'Infinite' || $scope.productQuantity == undefined){
               $scope.apiproductQuantity = -1;
@@ -41,14 +55,14 @@
               $scope.apiproductQuantity =$scope.productQuantity;
             }
             var query = productFactory.editProduct({
-              "prodId":editProductId,
+              "prodId":edit_Product_Id,
               "pname": $scope.productName,
               "pdesc":$scope.productDescription,
               "price":$scope.productPrice,
               "pinv":$scope.apiproductQuantity,
               "pinvdaily":false,
               "pcal":false,
-              "pimages":["353473874.jpg"],
+              "pimages":uploadResponseFileName,
               "pfeatures":false,
               "lid": lid
             });
@@ -56,13 +70,23 @@
                         console.log(data);
                         $state.go('dashboard.productList');
                     });
-          };
+          }
 
 
-       		$scope.addProduct = function(){
-            $scope.spinner = true;
-            $scope.productDescription = angular.element(document.querySelector('#uikit_editor_2')).val();
-            var userData = localStorageService.get('userData');
+          $scope.addProduct = function(){
+            if($scope.picImage==after_load_image_response){
+                  send_data_after_uploader_response();
+         
+            }else{
+                  $scope.spinner = true;
+                   upload($scope.picImage, 'https://protected-badlands-3499.herokuapp.com/prodfile');
+                 }
+
+
+               };
+           
+           function send_data_after_uploader_response(){
+                 var userData = localStorageService.get('userData');
             var lid = userData.locations[0];
             if($scope.productQuantity == 'Infinite' || $scope.productQuantity == undefined){
               $scope.apiproductQuantity = -1;
@@ -77,36 +101,41 @@
               "pinv":$scope.apiproductQuantity,
               "pinvdaily":false,
               "pcal":false,
-              // "pimages":[data.filename],
-              "pimages":['320145.jpg'],
+              "pimages":uploadResponseFileName,
               "pfeatures":false,
               "lid": lid
             });
       query.$promise.then(function(data) {
-                        console.log(data);
+                       // console.log(data);
                         $scope.spinner = false;
                         $scope.productName = '';
                         $scope.productPrice = '';
                         $scope.productQuantity= '';
                         angular.element(document.querySelector('.CodeMirror-code pre span')).text(' ');
                     });
-                       // $state.go('dashboard.productList');
-                        upload($scope.picImage, 'https://protected-badlands-3499.herokuapp.com/prodfile');
-       		console.log($scope.picImage);
-          };
-          
+                        $state.go('dashboard.productList');
+                       
+           }
            // upload on file select or drop
+       var uploadResponseFileName;    
      function upload (file, url) {
         Upload.upload({
             url: url,
             data: {fileName: file}
         }).then(function (resp) {
-            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+            uploadResponseFileName = resp.data.filename;
+            console.log(uploadResponseFileName);
+            if(flag_for_cheking_add_or_edit==1){
+              edit_product_after_uploader_response();
+            }else{
+            send_data_after_uploader_response();
+          }
+            //console.log('Success ' + resp.config.data.file + 'uploaded. Response: ' + resp.data);
         }, function (resp) {
-            console.log('Error status: ' + resp.status);
+           // console.log('Error status: ' + resp.status);
         }, function (evt) {
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+           // console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file);
         });
     };
 
