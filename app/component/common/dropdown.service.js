c47f447936ec70e34cf11f8728af6bb37dfa84fe
rahulexcel <yuvraj@excellencetechnologies.in>
2016-01-12 12:54:42 +0530

    Populate country currency as per selected country.

--- app/component/common/dropdown.service.js
+++ app/component/common/dropdown.service.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('xenon-app')
         .factory('dropdownService', dropdownService);
-    function dropdownService() {
+    function dropdownService(country, countryData) {
         var service = {};
         service.Daydropdown = function() {
             var Daydropdown=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -18,6 +18,19 @@
                }
                return hour;
         }
+         service.countryDropdown = function(){
+             // var countryCode = [];
+
+              var countryName = [];
+              for (var i = 0; i < countryData.length; i++) {
+              // countryCode.push(country[i].code);
+               countryName.push(countryData[i].name.common);
+                   // console.log(countryData[i].name.common.length);
+                   }
+
+            return countryName;
+        }
+
         return service;
     }
 })();
--- app/setting/setting.controller.js
+++ app/setting/setting.controller.js
@@ -3,27 +3,35 @@
 
     angular.module('xenon-app')
             .controller('settingController', settingController);
-    function settingController($scope, storeinfoLocationsIdFactory, localStorageService) {
+    function settingController($scope, countryData, dropdownService, storeinfoLocationsIdFactory, localStorageService) {
         console.log("Setting Page");
         var userData = localStorageService.get("userData");
         var locationid = userData.locations[0];
-        var query = storeinfoLocationsIdFactory.update({}, {
+        $scope.countryName =  dropdownService.countryDropdown();
+        $scope.selectedCountry = function() {
+   for (var i = 0; i < countryData.length; i++) {
+    if ($scope.selectedCountryName == countryData[i].name.common) {
+     $scope.countryCurrency = countryData[i].currency;
+    }
+   }
+  };
+        var query = storeinfoLocationsIdFactory.get({}, {
             'locationid': locationid
         });
         query.$promise.then(function(data) {
             console.log(data);
-            $scope.countryName = data.data.lcountry;
-            $scope.countryCurrency = data.data.lcurrency;
-            $scope.tax = data.data.ltax;
-            $scope.includeTax = data.data.ltaxall;
-            $scope.transalation = data.data.lstorelang;
+            $scope.selectedCountryName = data.lcountry;
+            console.log($scope.selectedCountryName);
+            $scope.countryCurrency = data.lcurrency;
+            $scope.tax = data.ltax;
+            $scope.includeTax = data.ltaxall;
+            $scope.transalation = data.lstorelang;
         });
         $scope.save = function() {
             $scope.spinner = true;
-            console.log($scope.countryName + $scope.countryCurrency + $scope.tax + $scope.includeTax + $scope.transalation);
             var query = storeinfoLocationsIdFactory.update({}, {
                 'locationid': locationid,
-                'lcountry': $scope.countryName,
+                'lcountry': $scope.selectedCountryName,
                 'lcurrency': $scope.countryCurrency,
                 'ltax': $scope.tax,
                 'ltaxall': $scope.includeTax,
