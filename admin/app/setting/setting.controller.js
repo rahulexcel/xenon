553fb47ecaa3e46af63f305ac26543e6f6990f38
rahul <prabhunath@excellencetechnologies.in>
2016-02-05 18:39:58 +0530

    MAJOR CHANGES DONE LIKE MOVING CALANDER ETC TO SETTING PAGES

--- admin/app/setting/setting.controller.js
+++ admin/app/setting/setting.controller.js
@@ -3,13 +3,19 @@
 
     angular.module('xenon-app')
             .controller('settingController', settingController);
-    function settingController($scope, countryData, dropdownService, storeinfoLocationsIdFactory, localStorageService) {
+    function settingController($scope, countryData, calanderService, dropdownService, storeinfoLocationsIdFactory, localStorageService) {
         console.log("Setting Page");
         var tz = jstz.determine();
         var timeZone = tz.name();
         var userData = localStorageService.get("userData");
         var locationid = userData.locations[0];
+        var dateArray = [];
+        var responseDateArr = [];
+        $scope.dropdown_days = dropdownService.Daydropdown();
+        $scope.openingtime_hour = dropdownService.Timedropdown();
+        $scope.closingtime_hour = dropdownService.Timedropdown();
         $scope.countryName = dropdownService.countryDropdown();
+        $scope.llt = 10;
         $scope.selectedCountry = function() {
             for (var i = 0; i < countryData.length; i++) {
                 if ($scope.selectedCountryName == countryData[i].name.common) {
@@ -22,7 +28,7 @@
         });
         query.$promise.then(function(data) {
             console.log(data);
-           
+
             $scope.selectedCountryName = data.lcountry;
             console.log($scope.selectedCountryName);
             $scope.countryCurrency = data.lcurrency;
@@ -35,7 +41,25 @@
             console.log(data.lcountry);
             $scope.selectedCountryName = data.lcountry;
             $scope.countryCurrency = data.lcurrency;
-
+            $scope.day_in_schedule_view = data.lwots;
+            $scope.llt = data.llt;
+            dayArr_for_schedule_view = $scope.day_in_schedule_view;
+            if ($scope.day_in_schedule_view.length == 0) {
+                $scope.show_scheduled_table = false;
+            } else {
+                $scope.show_scheduled_table = true;
+            }
+            var closed = data.ldateclosed;
+            dateArray = dateArray.concat(closed);
+            for (var i = 0; i < closed.length; i++) {
+                var responseDate = closed[i].split('-').reverse();
+                console.log(responseDate);
+                var responseTimestamp = new Date(responseDate).getTime();
+                responseDateArr.push(responseTimestamp);
+                console.log(responseTimestamp);
+            }
+            $scope.selectedDays = responseDateArr;
+            console.log($scope.selectedDays);
         });
         $scope.save = function() {
             $scope.spinner = true;
@@ -46,17 +70,75 @@
                 'ltax': $scope.tax,
                 'ltaxall': $scope.includeTax,
                 'lstorelang': $scope.transalation,
+                'ldateclosed': dateArray,
                 'ldeliverymode': $scope.deliveryMode,
                 'ldeliveryprice': $scope.deliveryPrice,
                 'ldeliverytax': $scope.deliveryTax,
-                'ltimezone': timeZone
+                'ltimezone': timeZone,
+                'lwots': dayArr_for_schedule_view,
+                'llt': $scope.llt
             });
             query.$promise.then(function(data) {
                 console.log(data);
-                  localStorageService.set('storeInfo', data);
+                localStorageService.set('storeInfo', data);
                 $scope.spinner = false;
             });
         };
+        $scope.logInfos = function(event, date) {
+            var ServiceDateArrayResponse = calanderService.getcalanderService(event, date, dateArray);
+            console.log(ServiceDateArrayResponse);
+        }
+        var removed_day_array_from_dropdown = [];
+        $scope.showOptions = function(row_from_dropdown) {
+            var flag = 0;
+            if (angular.isDefined($scope.day_in_schedule_view)) {
+                for (var i = 0; i < $scope.day_in_schedule_view.length; i++) {
+                    if (row_from_dropdown == $scope.day_in_schedule_view[i].day) {
+                        removed_day_array_from_dropdown.push(row_from_dropdown);
+                        flag = 1;
+                    } else {
+                    }
+                }
+            } else {
+                return true;
+            }
+            if (flag == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        var dayArr_for_schedule_view = [];
+        $scope.tsave = function() {
+            if ($scope.day && $scope.opening_selected_hour && $scope.closing_selected_hour) {
+
+                $scope.show_scheduled_table = true;
+                var day = $scope.day;
+                var openingTime = $scope.opening_selected_hour;
+                var closingTime = $scope.closing_selected_hour;
+                var json;
+                json = {
+                    'day': day,
+                    'opening_time': openingTime,
+                    'closing_time': closingTime
+                }
+                dayArr_for_schedule_view.unshift(json);
+                $scope.day_in_schedule_view = dayArr_for_schedule_view;
+                $scope.opening_selected_hour = '';
+                $scope.closing_selected_hour = "";
+                $scope.day = '';
+            }
+        }
+        $scope.removeTimes = function(index) {
+            var idx = dayArr_for_schedule_view.indexOf(index);
+            dayArr_for_schedule_view.splice(idx, 1);
+            if ($scope.day_in_schedule_view.length == 0) {
+                $scope.show_scheduled_table = false;
+            } else {
+                $scope.show_scheduled_table = true;
+            }
+
+        }
     }
     ;
 
--- admin/app/storeinfo/storeinfo.controller.js
+++ admin/app/storeinfo/storeinfo.controller.js
@@ -6,31 +6,19 @@
     function storeinfoCtrl($scope, $log, countryData, FileUploader, arrayService, uploadService, dropdownService, $state, storeinfoFactory, $timeout, calanderService, localStorageService, Upload, storeinfoLocationsFactory, storeinfoLocationsIdFactory, storeinfoLocFile) {
         var tz = jstz.determine();
         var timeZone = tz.name();
-        var dateArray = [];
-        var responseDateArr = [];
         var i;
         var LocationIdFlag = 0;
         var userData = localStorageService.get("userData");
         var locationId = userData.locations[0];
-        var dayArr_for_schedule_view = [];
         var response_phone_no;
         var response_pic_name;
         var uploadResponseFileName;
-        $scope.llt = 10;
-        $scope.dropdown_days = dropdownService.Daydropdown();
-        $scope.openingtime_hour = dropdownService.Timedropdown();
-        $scope.closingtime_hour = dropdownService.Timedropdown();
         $scope.dropdown_country = dropdownService.countryDropdown();
         if (angular.isDefined(locationId)) {
             On_refresh();
         } else {
             LocationIdFlag = 1;
         }
-        $scope.logInfos = function(event, date) {
-            var ServiceDateArrayResponse = calanderService.getcalanderService(event, date, dateArray);
-            console.log(ServiceDateArrayResponse);
-        }
-
         function On_refresh() {
             $scope.spinner = true;
             var query = storeinfoLocationsIdFactory.get({}, {
@@ -50,63 +38,22 @@
                 $scope.lcity = data.lcity;
                 $scope.lstate = data.lstate;
                 $scope.lcountry = data.lcountry;
-                $scope.llt = data.llt;
                 $scope.lmessage = data.lmessage;
                 $scope.highlightDays = data.ldateclosed;
-                $scope.day_in_schedule_view = data.lwots;
                 $scope.lclosed = data.lclosed;
-                if ($scope.day_in_schedule_view.length == 0) {
-                    $scope.show_scheduled_table = false;
-                } else {
-                    $scope.show_scheduled_table = true;
-                }
                 response_phone_no = data.lphone;
                 console.log(data.lphone);
                 if (angular.isDefined(data.llogo)) {
                     $scope.picImage = 'http://s3.amazonaws.com/ordermagic/' + data.llogo;
                 }
                 response_pic_name = $scope.picImage;
-
-                $scope.phone_code =data.lcountrycode;
-                $scope.phone_no=data.lphone;
-
-                dayArr_for_schedule_view = $scope.day_in_schedule_view;
-                var closed = data.ldateclosed;
-                dateArray = dateArray.concat(closed);
-                for (i = 0; i < closed.length; i++) {
-                    var responseDate = closed[i].split('-').reverse();
-                    console.log(responseDate);
-                    var responseTimestamp = new Date(responseDate).getTime();
-                    responseDateArr.push(responseTimestamp);
-                    console.log(responseTimestamp);
-
-                }
-                $scope.selectedDays = responseDateArr;
+                $scope.phone_code = data.lcountrycode;
+                $scope.phone_no = data.lphone;
             });
-        }
-        var removed_day_array_from_dropdown = [];
-        $scope.showOptions = function(row_from_dropdown) {
-            var flag = 0;
-            if (angular.isDefined($scope.day_in_schedule_view)) {
-                for (var i = 0; i < $scope.day_in_schedule_view.length; i++) {
-                    if (row_from_dropdown == $scope.day_in_schedule_view[i].day) {
-                        removed_day_array_from_dropdown.push(row_from_dropdown);
-                        flag = 1;
-                    } else {
-                    }
-                }
-            } else {
-                return true;
-            }
-            if (flag == 0) {
-                return true;
-            } else {
-                return false;
-            }
         }
         $scope.lsave = function(picImageurl) {
             $scope.spinner = true;
-            
+
             if ($scope.picImage == response_pic_name) {
                 send_data_after_upload();
             } else {
@@ -119,12 +66,10 @@
             }
         }
         function send_data_after_upload() {
-       
             if (LocationIdFlag === 0) {
                 var query = storeinfoLocationsIdFactory.update({}, {
                     'locationid': userData.locations[0],
                     'lname': $scope.lname,
-                    'lwots': dayArr_for_schedule_view,
                     'ldesc': $scope.ldesc,
                     'lemail': $scope.lemail,
                     'laddr': $scope.laddr,
@@ -133,10 +78,8 @@
                     'lcity': $scope.lcity,
                     'lstate': $scope.lstate,
                     'lcountry': $scope.lcountry,
-                    'lcountrycode':$scope.phone_code[0],
+                    'lcountrycode': $scope.phone_code[0],
                     'lphone': $scope.phone_no,
-                    'llt': $scope.llt,
-                    'ldateclosed': dateArray,
                     'lmessage': $scope.lmessage,
                     'lclosed': $scope.lclosed,
                     'ltimezone': timeZone
@@ -150,19 +93,16 @@
                 var query = storeinfoFactory.save({
                     'locationId': userData.locations[0],
                     'lname': $scope.lname,
-                    'lwots': dayArr_for_schedule_view,
                     'ldesc': $scope.ldesc,
                     'lemail': $scope.lemail,
                     'llogo': uploadResponseFileName,
                     'laddr': $scope.laddr,
                     'lpostcode': $scope.lpostcode,
                     'lcity': $scope.lcity,
-                    'ldateclosed': dateArray,
                     'lstate': $scope.lstate,
-                  'lcountrycode':$scope.phone_code[0],
+                    'lcountrycode': $scope.phone_code[0],
                     'lcountry': $scope.lcountry,
-                   'lphone': $scope.phone_no,
-                    'llt': $scope.llt,
+                    'lphone': $scope.phone_no,
                     'lmessage': $scope.lmessage,
                     'lclosed': $scope.lclosed,
                     'ltimezone': timeZone
@@ -175,36 +115,6 @@
                 });
             }
         }
-        $scope.tsave = function() {
-            if($scope.day && $scope.opening_selected_hour && $scope.closing_selected_hour){
-             
-            $scope.show_scheduled_table = true;
-            var day = $scope.day;
-            var openingTime = $scope.opening_selected_hour;
-            var closingTime = $scope.closing_selected_hour;
-            var json;
-            json = {
-                'day': day,
-                'opening_time': openingTime,
-                'closing_time': closingTime
-            }
-            dayArr_for_schedule_view.unshift(json);
-            $scope.day_in_schedule_view = dayArr_for_schedule_view;
-            $scope.opening_selected_hour = '';
-            $scope.closing_selected_hour = "";
-            $scope.day = '';
-            } 
-        }
-        $scope.removeTimes = function(index) {
-            var idx = dayArr_for_schedule_view.indexOf(index);
-            dayArr_for_schedule_view.splice(idx, 1);
-            if ($scope.day_in_schedule_view.length == 0) {
-                $scope.show_scheduled_table = false;
-            } else {
-                $scope.show_scheduled_table = true;
-            }
-
-        }
         $scope.country_selected = function() {
             for (var i = 0; i < countryData.length; i++) {
                 if ($scope.lcountry == countryData[i].name.common) {
@@ -213,18 +123,18 @@
                 }
             }
         }
-        $scope.checkStoreSetting = function(){
+        $scope.checkStoreSetting = function() {
             // console.log('checkStoreSetting');
-            if(localStorageService.get('storeInfo')){
+            if (localStorageService.get('storeInfo')) {
                 var storeInfo = localStorageService.get('storeInfo');
                 var lcompletedLength = storeInfo.lcompleted.length;
-                if(lcompletedLength <3){
+                if (lcompletedLength < 3) {
                     $scope.checkStoreOpen = true;
-                    $scope.lclosed = false; 
+                    $scope.lclosed = false;
                 }
-            } else{
+            } else {
                 $scope.checkStoreOpen = true;
-                $scope.lclosed = false;                
+                $scope.lclosed = false;
             }
         }
     }
